178ae5e4aa3dedadbf6bcfa46ee73504f5eccad2
thetrickuser <coolady1807@outlook.com>
2026-01-07 13:53:13 +0530

    initial commit

---
+++ ordersphere-bom/src/test/java/com/ordersphere/orderspherebom/OrdersphereBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordersphere.orderspherebom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersphereBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
